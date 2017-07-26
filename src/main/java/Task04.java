f3a3249e90ac57a7245e032f59873c0b31db9707
Evgenii Sidorenko <Evgenii_Sidorenko@epam.com>
2017-07-26 19:06:45 +0300

    Task04 complete

---
+++ src/main/java/Task04.java
@@ -0,0 +1,20 @@
+public class Task04 {
+    private static class Node {
+        int data;
+        Node next;
+
+        boolean hasCycle(Node head) {
+            Node current = head;
+            Node prev = head;
+            while (current != null && current.next != null) {
+                prev = prev.next;
+                current = current.next.next;
+                if (prev == current) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+}
