dffaf2d671bc2eecb9c73461bde1675eae99f95e
Evgenii Sidorenko <Evgenii_Sidorenko@epam.com>
2017-07-27 06:17:36 +0300

    Task06 complete

---
+++ src/main/java/Task06.java
@@ -0,0 +1,24 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class Task06 {
+    public static void main(String[] args) {
+        Deque<Integer> queue = new ArrayDeque<>();
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int operation = scan.nextInt();
+            if (operation == 1) { // enqueue
+                queue.addLast(scan.nextInt());
+            } else if (operation == 2) { // dequeue
+                queue.pollFirst();
+            } else if (operation == 3) { // print/peek
+                System.out.println(queue.getFirst());
+            }
+        }
+        scan.close();
+    }
+}
