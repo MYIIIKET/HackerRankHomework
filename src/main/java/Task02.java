b77d0cc38a52fac41aca68cb42d423e7c877f488
Evgenii Sidorenko <Evgenii_Sidorenko@epam.com>
2017-07-29 10:33:54 +0300

    Task02 complete

---
+++ src/main/java/Task02.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+import static java.lang.String.CASE_INSENSITIVE_ORDER;
+
+class Player {
+    String name;
+    int score;
+
+    Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class Checker implements Comparator<Player> {
+    @Override
+    public int compare(Player o1, Player o2) {
+        return ((o2.score - o1.score) != 0) ? (o2.score - o1.score) : CASE_INSENSITIVE_ORDER.compare(o1.name, o2.name);
+    }
+}
+
+public class Task02 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+
+        for (int i = 0; i < n; i++) {
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for (int i = 0; i < player.length; i++) {
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
+
+
