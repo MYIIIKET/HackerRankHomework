12cfddb9fa325f510f87f3ccfe673ce442260bef
Evgenii Sidorenko <Evgenii_Sidorenko@epam.com>
2017-08-03 23:15:28 +0300

    fix

--- src/main/java/Pack01/Task03.java
+++ src/main/java/Pack01/Task03.java
@@ -4,25 +4,37 @@ import java.util.*;
 
 public class Task03 {
     public static void main(String[] args) {
+        Map<String, Integer> words = new HashMap();
         Scanner in = new Scanner(System.in);
         int m = in.nextInt();
         int n = in.nextInt();
-        String magazine[] = new String[m];
+        String word = "";
+        int counts = 0;
         for (int magazine_i = 0; magazine_i < m; magazine_i++) {
-            magazine[magazine_i] = in.next();
+            word = in.next();
+            if (words.containsKey(word)) {
+                counts = words.get(word);
+                words.put(word, ++counts);
+            } else {
+                words.put(word, 1);
+            }
         }
-        String ransom[] = new String[n];
+        boolean flag = true;
         for (int ransom_i = 0; ransom_i < n; ransom_i++) {
-            ransom[ransom_i] = in.next();
+            word = in.next();
+            if (words.containsKey(word)) {
+                counts = words.get(word);
+                if (--counts == 0) {
+                    words.remove(word);
+                } else {
+                    words.put(word, counts);
+                }
+            } else {
+                flag = false;
+                break;
+            }
         }
-
-        List<String> magazineList = new ArrayList<>();
-        List<String> ransomList = new ArrayList<>();
-
-        Arrays.stream(magazine).forEach(s -> magazineList.add(s));
-        Arrays.stream(ransom).forEach(s -> ransomList.add(s));
-
-        if (!ransomList.stream().filter(value -> !magazineList.remove(value)).findFirst().isPresent()) {
+        if (flag) {
             System.out.println("Yes");
         } else {
             System.out.println("No");
